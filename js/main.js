0db57f7f4ce5f718e074f066c7f113ca8ea97472
Philip Barton <35851000+PhilipBarton@users.noreply.github.com>
2018-09-15 10:54:07 -0230

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@
 					var post 	= $(this),
 						bg 		= post.data('bg');
 
-					post.css( 'background-image', 'url(images/' + bg + ')' );
+					post.css( 'background-image', 'url' + bg + ')' );
 
 				});
 
